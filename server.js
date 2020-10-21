634980ecad7f168b94776f7588ca91421be04f07
ayoub ar <ayoubarahmat9@gmail.com>
2020-10-21 23:59:22 +0100

    last update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(express.static('./dist'));
+app.use(express.static('./dist/fronthackajob'));
 app.get('/*', function (req, res) {
- res.sendFile(__dirname + '/dist/index.html');
+ res.sendFile(__dirname + '/dist/fronthackajob/index.html');
 });
 app.listen(process.env.PORT || 8080);
